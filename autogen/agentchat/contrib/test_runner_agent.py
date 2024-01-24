63211ccbda2963fb23f69c0397d7d3880fb34f22
Gabe Gralla <ggralla@users.noreply.github.com>
2024-01-24 21:38:29 +0000

    Add TestRunnerAgent and TestWriterAgent classes

---
+++ autogen/agentchat/contrib/test_runner_agent.py
@@ -0,0 +1,34 @@
+import subprocess
+import autogen
+from typing import Dict, Union
+
+
+class TestRunnerAgent(autogen.UserProxyAgent):
+    def __init__(self, name: str, **kwargs):
+        super().__init__(name, **kwargs)
+
+    def generate_init_message(self, *args, **kwargs) -> Union[str, Dict]:
+        return (
+            super().generate_init_message(*args, **kwargs)
+            + """
+All tests will be checked run by running the pytest CLI tool."""
+        )
+
+    def run_code(self, code, **kwargs):
+        language = kwargs.get("lang")
+        if language != "python":
+            raise Exception(f"Unsupported lanuage {language}")
+        filename = "latest_tests.py"
+        with open(filename, "w") as f:
+            f.write(code)
+        try:
+            result = subprocess.run(["pytest", filename], check=True, text=True, capture_output=True)
+            if result.returncode:
+                logs = result.stderr
+
+            else:
+                logs = result.stdout
+            return result.returncode, logs, None
+        except subprocess.CalledProcessError as err:
+            print(f"Tests failed with error: {err.stderr} {err.stdout}")
+            return err.returncode, err.stderr, None
--- autogen/agentchat/contrib/test_writer_agent.py
+++ autogen/agentchat/contrib/test_writer_agent.py
@@ -1,11 +1,8 @@
 from typing import List, Optional
 from autogen.agentchat.conversable_agent import ConversableAgent
 
-import re
-import pandas as pd
 
-
-class TestAgent(ConversableAgent):
+class TestWriterAgent(ConversableAgent):
     def __init__(
         self,
         name: Optional[str] = "testagent",
@@ -14,6 +11,7 @@ class TestAgent(ConversableAgent):
     ):
         if not system_message:
             system_message = """You are a software engineer, who is an expert on how to write python tests using pytest.
-            You are very meticulous, and always write through tests for code. You make sure to write tests that cover all funcitonality, and avoid using mocks when integration tests are possible."""
+            You are very meticulous, and always write thorough tests for code. You make sure to write tests that cover all functionality, and avoid using mocks when integration tests are possible.
+            Only write python code. Assume all packages are installed."""
 
         super().__init__(name, system_message=system_message, **kwargs)
